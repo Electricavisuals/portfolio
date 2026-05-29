504ac7768ddc7f4f7308a27f9843fb547bc17531
Electricavisuals <albertcallejo@gmail.com>
2026-05-30 00:39:37 +0200

    update

---
+++ push.bat
@@ -0,0 +1,6 @@
+@echo off
+echo Pujant a GitHub...
+powershell -Command "cd '\\MSI\d\CV\3D_Videojocs\PortfolioWeb'; git add .; git commit -m 'update'; git push"
+echo.
+echo Fet! https://Electricavisuals.github.io/portfolio
+pause
